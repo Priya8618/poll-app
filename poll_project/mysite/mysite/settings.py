fcf35246ab7633ad4997e16862b7ff2505fdf4fc
Priya S <priyasomannapriya@gmail.com>
2026-02-26 15:47:45 +0530

    md

--- poll_project/mysite/mysite/settings.py
+++ poll_project/mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u0)_co+#i4iqfvfupr12sy+i_4%cpq2wha$o3*a2brt@ao6+ts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,4 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
